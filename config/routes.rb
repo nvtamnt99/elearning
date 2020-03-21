f530f57698e0022f663da2da717fbd792db1868e
Tam <nvtamnt99@gmail.com>
2020-03-21 10:40:53 +0700

    add slider

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   post    "login"    => "sessions#create"
   delete  "logout"   => "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-   root 'welcome#index'
+   root 'static_pages#home'
    get 'users/sign_up' => 'users#new'
    post 'users/sign_up' => 'users#create'
    resources :users, only: [:show]
