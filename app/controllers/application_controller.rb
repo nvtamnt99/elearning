a0d52be4e4779d14cfb4f688d14122ac7fac52e8
Tam <nvtamnt99@gmail.com>
2020-03-24 15:01:29 +0700

    refactor code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
-  
-  helper_method :current_user
-  helper_method :log_in?
-
-  def current_user
-    User.find_by(id: session[:user_id])
-  end
-
-  def log_in?
-    !current_user.nil?
-  end
 end
