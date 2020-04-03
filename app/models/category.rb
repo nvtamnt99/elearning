f874993f29db724ccb157e9dae0a2cd0ee4ed3d5
Tam <nvtamnt99@gmail.com>
2020-04-03 21:02:14 +0700

    add association

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   validates :name, presence: true
+  belongs_to :lesson
+  belongs_to :word  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   mount_uploader :avatar, AvatarUploader
   has_secure_password
+  belongs_to :relationship
+  has_many :lessons
 end
