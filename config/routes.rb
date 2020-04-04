91634357e99dce265d0a97dff3f12d153d877ede
Tam <nvtamnt99@gmail.com>
2020-04-04 21:58:48 +0700

    admin pages

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
    namespace :admin do
      resources :categories, only: [:new, :create]
      resources :words, only: [:index, :new, :create]
-     resources :admin_pages, only: [:home]
+     root 'admin_pages#home'
    end
 end
