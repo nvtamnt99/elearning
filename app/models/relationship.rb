066133cffa8d9196d4359a0635f5922ab7d11f75
Tam <nvtamnt99@gmail.com>
2020-04-04 17:29:42 +0700

    refactor association

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,3 +1,3 @@
 class Relationship < ApplicationRecord
-  has_and_belongs_to_many :user
+  has_and_belongs_to_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   mount_uploader :avatar, AvatarUploader
   has_secure_password
-  has_and_belongs_to_many :relationship
+  has_and_belongs_to_many :relationships
   has_many :lessons
 end
